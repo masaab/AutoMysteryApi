5c45c9ec69e8517aa58c434bba4ffe49088b63fd
masaab <masaabmushtaq@gmail.com>
2020-10-05 15:51:16 +1000

    new changes

--- WebApplication2/Controllers/ValuesController.cs
+++ WebApplication2/Controllers/ValuesController.cs
@@ -20,42 +20,32 @@ namespace WebApplication2.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        public string Get(int id)
+        public string Get(string id)
         {
-            //ChangeB2GPasswordResponseMessage message = null;
             try
             {
-                string url = "https://b2g-disc.ppsr.gov.au/PpsrB2GService/2016/05/RegisterOperations.svc/soap11";
-              
+                string url = "https://b2g-disc.ppsr.gov.au/PpsrB2GService/2016/05/CollateralRegistrationSearch.svc/soap11";
+
                 string username = "MUS258", password = "TearOfSun1$";
-               
-                var operationClient = PPSRService.CreatePPSROperationClientProxy(url, username, password);
+                SearchBySerialNumberResponseMessage message = null;
+                var operationClient = PPSRService.CreatePPSRClient(url, username, password);
                 var operationChannel = operationClient.ChannelFactory.CreateChannel();
                 using (new OperationContextScope((IClientChannel)operationChannel))
                 {
-                    var tt = operationClient.Endpoint;
-                    //var message = operationChannel.ChangeB2GPassword(new ChangeB2GPasswordRequestMessage
-                    //{
-                    //    TargetEnvironment = development,
-                    //    ChangeB2GPasswordRequest = new ChangeB2GPasswordRequestType
-                    //    {
-                    //        Username = "MUS258",
-                    //        NewPassword = "TearOfSun1$1",
-                    //        CustomersRequestMessageId = Guid.NewGuid().ToString()
-                    //    }
-                    //});
-
-                    var message = operationChannel.Ping(new PingRequestMessage
+                    message = operationChannel.SearchBySerialNumber(new SearchBySerialNumberRequestMessage
                     {
                         TargetEnvironment = development,
-                        PingRequest = new PingRequestType
+                        SearchBySerialNumberRequest = new SearchBySerialNumberRequestType
                         {
-                            CustomersRequestMessageId = Guid.NewGuid().ToString()
+                            CustomersRequestMessageId = Guid.NewGuid().ToString(),
+                            SearchCriteria = new SearchBySerialNumberSearchCriteria
+                            {
+                                IncludeCurrent = true,
+                                SerialNumberType = SerialNumberSearchCriteriaType.VIN,
+                                SerialNumber = id//"JT752EP9100474959"//"JS1SP46A000504266"
+                            }
                         }
                     });
-
-
                     return JsonConvert.SerializeObject(message);
                 }
             }
@@ -65,20 +55,49 @@ namespace WebApplication2.Controllers
             }
         }
 
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-           
-        }
+       
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //public string Get(int id)
+        //{
+        //    //ChangeB2GPasswordResponseMessage message = nul    l;
+        //    try
+        //    {
+        //        string url = "https://b2g-disc.ppsr.gov.au/PpsrB2GService/2016/05/RegisterOperations.svc/soap11";
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+        //        string username = "MUS258", password = "TearOfSun1$";
+
+        //        var operationClient = PPSRService.CreatePPSROperationClientProxy(url, username, password);
+        //        var operationChannel = operationClient.ChannelFactory.CreateChannel();
+        //        using (new OperationContextScope((IClientChannel)operationChannel))
+        //        {
+        //            //var message = operationChannel.ChangeB2GPassword(new ChangeB2GPasswordRequestMessage
+        //            //{
+        //            //    TargetEnvironment = development,
+        //            //    ChangeB2GPasswordRequest = new ChangeB2GPasswordRequestType
+        //            //    {
+        //            //        Username = "MUS258",
+        //            //        NewPassword = "TearOfSun1$1",
+        //            //        CustomersRequestMessageId = Guid.NewGuid().ToString()
+        //            //    }
+        //            //});
+
+        //            var message = operationChannel.Ping(new PingRequestMessage
+        //            {
+        //                TargetEnvironment = development,
+        //                PingRequest = new PingRequestType
+        //                {
+        //                    CustomersRequestMessageId = Guid.NewGuid().ToString()
+        //                }
+        //            });
+
+
+        //            return JsonConvert.SerializeObject(message);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return $"MESSAGE: {ex?.Message}  STACETRACE: {ex?.StackTrace}  SOURCE: {ex?.Source}  INNER EXCEPTION: {ex?.InnerException?.ToString()}";
+        //    }
+        //}
     }
 }
