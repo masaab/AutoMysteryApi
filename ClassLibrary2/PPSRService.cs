ee4837330750d174d5b881362f8f9f99d24689f9
masaab <masaabmushtaq@gmail.com>
2020-10-07 14:29:28 +1000

    new changes

--- ClassLibrary2/PPSRService.cs
+++ ClassLibrary2/PPSRService.cs
@@ -2,15 +2,18 @@
 using ClassLibrary2.ServiceReference1;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
+using System.ServiceModel.Configuration;
 using System.ServiceModel.Security;
+using System.Text;
 
 namespace ClassLibrary2
 {
+    public enum UrlType { MTOM,normal }
     public class PPSRService
     {
         public static CollateralRegistrationSearchService2016Client CreatePPSRClient(string url,
                                                                 string username,
-                                                                string password)
+                                                                string password, UrlType urlType)
         {
             CustomBinding binding = new CustomBinding();
 
@@ -18,10 +21,54 @@ namespace ClassLibrary2
             security.IncludeTimestamp = false;
             security.DefaultAlgorithmSuite = SecurityAlgorithmSuite.Basic256;
             security.MessageSecurityVersion = MessageSecurityVersion.WSSecurity10WSTrustFebruary2005WSSecureConversationFebruary2005WSSecurityPolicy11BasicSecurityProfile10;
+            MessageEncodingBindingElement encoding = null;
+            if (urlType == UrlType.MTOM)
+            {
+                encoding = new MtomMessageEncodingBindingElement();
+                encoding.MessageVersion = MessageVersion.Soap11;
+            }
+            else
+            {
+                encoding = new TextMessageEncodingBindingElement();
+                encoding.MessageVersion = MessageVersion.Soap11;
+            }
 
-            var encoding = new TextMessageEncodingBindingElement();
-            encoding.MessageVersion = MessageVersion.Soap11;
+            var transport = new HttpsTransportBindingElement();
+            transport.MaxReceivedMessageSize = 20000000; // 20 megs
+            
+            binding.Elements.Add(security);
+            binding.Elements.Add(encoding);
+            binding.Elements.Add(transport);
 
+            CollateralRegistrationSearchService2016Client client = new CollateralRegistrationSearchService2016Client(binding,
+                new EndpointAddress(url));
+
+            // to use full client credential with Nonce uncomment this code:
+            // it looks like this might not be required - the service seems to work without it
+            // client.ChannelFactory.Endpoint.Behaviors.Add(new InspectorBehavior());
+            client.ChannelFactory.Endpoint.Behaviors.Remove<System.ServiceModel.Description.ClientCredentials>();
+            client.ChannelFactory.Endpoint.Behaviors.Add(new CustomCredentials());
+
+            client.ClientCredentials.UserName.UserName = username;
+            client.ClientCredentials.UserName.Password = password;
+
+            return client;
+        }
+
+        public static CollateralRegistrationSearchService2016Client CreatePPSRClient1(string url,
+                                                               string username,
+                                                               string password)
+        {
+            CustomBinding binding = new CustomBinding();
+
+            var security = TransportSecurityBindingElement.CreateUserNameOverTransportBindingElement();
+            security.IncludeTimestamp = false;
+            security.DefaultAlgorithmSuite = SecurityAlgorithmSuite.Basic256;
+            security.MessageSecurityVersion = MessageSecurityVersion.WSSecurity10WSTrustFebruary2005WSSecureConversationFebruary2005WSSecurityPolicy11BasicSecurityProfile10;
+            MessageEncodingBindingElement encoding = null;
+
+            encoding = new MtomMessageEncodingBindingElement(MessageVersion.Soap11, Encoding.UTF8);
+            
             var transport = new HttpsTransportBindingElement();
             transport.MaxReceivedMessageSize = 20000000; // 20 megs
 
--- WebApplication2/Controllers/ValuesController.cs
+++ WebApplication2/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using ClassLibrary2.ServiceReference1;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.ServiceModel;
 using System.Web.Http;
 
@@ -23,10 +24,14 @@ namespace WebApplication2.Controllers
             try
             {
                 string url = "https://b2g-disc.ppsr.gov.au/PpsrB2GService/2016/05/CollateralRegistrationSearch.svc/soap11";
-
+                string urlMTOM = "https://b2g-disc.ppsr.gov.au/PpsrB2GService/2016/05/CollateralRegistrationSearch.svc/soap11mtom";
                 string username = "MUS258", password = "TearOfSun1$";
+
                 SearchBySerialNumberResponseMessage message = null;
-                var operationClient = PPSRService.CreatePPSRClient(url, username, password);
+                RequestSearchCertificateResponseMessage dsd = null;
+                RetrieveSearchCertificateResponseMessage ee = null;
+
+                var operationClient = PPSRService.CreatePPSRClient(url, username, password, UrlType.normal);
                 var operationChannel = operationClient.ChannelFactory.CreateChannel();
                 using (new OperationContextScope((IClientChannel)operationChannel))
                 {
@@ -37,15 +42,47 @@ namespace WebApplication2.Controllers
                         {
                             CustomersRequestMessageId = Guid.NewGuid().ToString(),
                             SearchCriteria = new SearchBySerialNumberSearchCriteria
-                            {
+                            { 
                                 IncludeCurrent = true,
                                 SerialNumberType = SerialNumberSearchCriteriaType.VIN,
                                 SerialNumber = id//"JT752EP9100474959"//"JS1SP46A000504266"
                             }
                         }
                     });
-                    return JsonConvert.SerializeObject(message);
+                   // return JsonConvert.SerializeObject(message);
                 }
+            
+                using (new OperationContextScope((IClientChannel)operationChannel))
+                {
+                    dsd = operationChannel.RequestSearchCertificate(new RequestSearchCertificateRequestMessage
+                    {
+                        TargetEnvironment = development,
+                        RequestSearchCertificateRequest = new RequestSearchCertificateRequestType
+                        {
+                            CustomersRequestMessageId = Guid.NewGuid().ToString(),
+                            SearchNumber = message.SearchBySerialNumberResponse.SearchSummary.SearchNumber,
+                            RegistrationNumber = message.SearchBySerialNumberResponse.SearchResult.ResultDetails.Last().RegistrationDetail.RegistrationNumber,
+                            ChangeNumber = message.SearchBySerialNumberResponse.SearchResult.ResultDetails.Last().RegistrationDetail.ChangeNumber
+                        }
+                    }) ;
+                }
+
+                //Create a new Client with new URL which uses MTOM
+                var operationClient1 = PPSRService.CreatePPSRClient1(urlMTOM, username, password);
+                var operationChannel1 = operationClient1.ChannelFactory.CreateChannel();
+                using (new OperationContextScope((IClientChannel)operationChannel1))
+                {
+                    ee = operationChannel1.RetrieveSearchCertificate(new RetrieveSearchCertificateRequestMessage
+                    {
+                        TargetEnvironment = development,
+                        RetrieveSearchCertificateRequest = new RetrieveSearchCertificateRequestType
+                        {
+                            CustomersRequestMessageId = Guid.NewGuid().ToString(),
+                            SearchCertificateNumber = dsd.RequestSearchCertificateResponse.SearchCertificateNumber
+                        }
+                    });
+                }
+                return JsonConvert.SerializeObject(ee);
             }
             catch (Exception ex)
             {
